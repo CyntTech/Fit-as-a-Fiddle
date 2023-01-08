16a1a0b3c68ec8502cb56970a8fea3f75ecfcf5c
CyntTech <101424204+CyntTech@users.noreply.github.com>
2023-01-08 13:54:30 +0100

    new update

---
+++ JQ.js
@@ -0,0 +1,9 @@
+$("document").ready(function(){
+
+    $("remedies_img").mouseenter(function(){
+        $("remedies_img").css('width','500px');
+    });
+
+    $("remedies_img").mouseleave(function(){
+        $("remedies_img").css('width','500px');
+    });
