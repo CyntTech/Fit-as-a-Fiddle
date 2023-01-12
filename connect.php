91c1790fb255c64fc6eb7b3ec228a2a6481b44f2
CyntTech <101424204+CyntTech@users.noreply.github.com>
2023-01-12 01:08:57 +0100

    files updated

---
+++ connect.php
@@ -0,0 +1,27 @@
+<?php
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $phone = $_POST['phone'];
+    $Gender = $_POST['Gender'];
+    $Country = $_POST['Country'];
+    $State = $_POST['State'];
+    $Email = $_POST['Email'];
+    $Password = $_POST['Password'];
+    $ConfirmPassword = $_POST['Confirm Password'];
+
+    //Database connection
+    $conn = new mysqli('localhost','root','','fitness');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(firstname, lastname, phone, gender, country, state, email, password, confirmPassword)
+            values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssissssss",$firstname, $lastname, $phone, $gender, $country, $state, $email, $password, $confirmPassword);
+        $stmt->execute();
+        echo "Registration Successfully..";
+        $stmt->close();
+        $conn->close();
+        
+        }
+    
+    ?>    
